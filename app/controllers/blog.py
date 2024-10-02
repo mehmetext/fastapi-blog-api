d179e74cdc014e56140829ada2e232d90d461a2e
Mehmet <mehmetext68@gmail.com>
2024-10-02 11:10:53 +0300

    blog controller added

---
+++ app/controllers/blog.py
@@ -0,0 +1,37 @@
+from enum import Enum
+from app.data.posts import example_posts
+
+
+class OrderBy(str, Enum):
+    title = "title"
+    content = "content"
+    created_at = "created_at"
+    updated_at = "updated_at"
+
+
+class BlogController:
+    def get_all_posts(
+        q: str | None = None,
+        author_id: int | None = None,
+        order_by: OrderBy | None = None,
+    ):
+        posts = example_posts
+
+        if q:
+            print(f"q: {q}")
+            posts = [
+                post
+                for post in posts
+                if q.lower() in post.title.lower() or q.lower() in post.content.lower()
+            ]
+        if author_id:
+            print(f"author_id: {author_id}")
+            posts = [post for post in posts if post.author_id == author_id]
+
+        print(f"Found posts count: {len(posts)}")
+
+        if order_by:
+            print(f"order_by: {order_by}")
+            posts = sorted(posts, key=lambda x: getattr(x, order_by.value))
+
+        return posts
--- app/routers/blog.py
+++ app/routers/blog.py
@@ -1,16 +1,8 @@
-from enum import Enum
 from fastapi import APIRouter
+from app.controllers.blog import BlogController, OrderBy
 from app.models.post import Post
-from app.data.posts import example_posts
 
 router = APIRouter(prefix="/blog", tags=["Blog"])
-
-
-class OrderBy(str, Enum):
-    title = "title"
-    content = "content"
-    created_at = "created_at"
-    updated_at = "updated_at"
 
 
 @router.get("/", response_model=list[Post])
@@ -19,23 +11,4 @@ async def get_all_posts(
     author_id: int | None = None,
     order_by: OrderBy | None = None,
 ):
-    posts = example_posts
-
-    if q:
-        print(f"q: {q}")
-        posts = [
-            post
-            for post in posts
-            if q.lower() in post.title.lower() or q.lower() in post.content.lower()
-        ]
-    if author_id:
-        print(f"author_id: {author_id}")
-        posts = [post for post in posts if post.author_id == author_id]
-
-    print(f"Found posts count: {len(posts)}")
-
-    if order_by:
-        print(f"order_by: {order_by}")
-        posts = sorted(posts, key=lambda x: getattr(x, order_by.value))
-
-    return posts
+    return BlogController.get_all_posts(q, author_id, order_by)
